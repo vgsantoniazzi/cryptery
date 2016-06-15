fd32cb97f38a3d0bc54eb87ed7b20e37dfaa7e6e
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2016-06-15 11:30:13 -0300

    Add processing with openssl md5

--- cryptery.c
+++ cryptery.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <openssl/md5.h>
 
 static const char alphabet[] =
   "abcdefghijklmnopqrstuvwxyz"
@@ -12,15 +13,26 @@ static const char alphabet[] =
 
 static const int alphabet_size = sizeof(alphabet) - 1;
 
-static const int max_chars = 10;
+static const int max_chars = 17;
 
-int current_number_chars = 1;
+int current_number_chars = 0;
 
 bool success_decrypt = false;
 
 char* md5_to_decrypt;
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+char* get_md5(const char *string) {
+  unsigned char result[MD5_DIGEST_LENGTH];
+  char md5_string[33];
+  MD5((unsigned char*)string, strlen(string),  (unsigned char*)&result);
+  int i;
+  for(i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    sprintf(&md5_string[i*2], "%02x", (unsigned int)result[i]);
+  }
+  return &md5_string;
+}
 
 void brute_force_token_matcher(char *str, int index, int max_size){
   for (int i = 0; i < alphabet_size; ++i){
@@ -29,7 +41,7 @@ void brute_force_token_matcher(char *str, int index, int max_size){
     }
     str[index] = alphabet[i];
     if (index == max_size - 1){
-      if(strcmp(md5_to_decrypt, str) == 0) {
+      if(strcmp(get_md5(str), md5_to_decrypt) == 0) {
         pthread_mutex_lock(&lock);
         printf("\nMD5 '%s' match as '%s'!\n", md5_to_decrypt, str);
         success_decrypt = true;
@@ -52,9 +64,11 @@ void brute_sequential(int max_length){
 }
 
 void *brute_match_md5(void *thread_id) {
+  int t = * (int *)thread_id;
   while(current_number_chars <= max_chars) {
     pthread_mutex_lock(&lock);
     current_number_chars += 1;
+    printf("\nThread #%d running algorithms with %d digits", t, current_number_chars);
     pthread_mutex_unlock(&lock);
     brute_sequential(current_number_chars);
   }
